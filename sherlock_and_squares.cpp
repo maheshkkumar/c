0c14f8972ca5c97aa6af4ae100ce2696548a2e6c
Mahesh Kumar K <maheshkumar.wdp21@gmail.com>
2015-05-01 22:01:08 +0530

    sherlock and squares

---
+++ sherlock_and_squares.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t;
+    int i;
+    cin>>t;
+    while(t--){
+        long long int a,b;
+        int count = 0;
+        cin>>a>>b;
+        a = ceil(sqrt(a));
+        b = floor(sqrt(b));
+        cout<<(int)(b - a) + 1<<endl;
+   }
+    return 0;
+}
